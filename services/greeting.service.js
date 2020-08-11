93e9df3d4e4923b4e8d0af648ede4a89b20225d9
bhavesh357 <bkadam357@gmail.com>
2020-08-11 22:42:17 +0530

    REFACTORED: to create a service class

--- services/greeting.service.js
+++ services/greeting.service.js
@@ -1,7 +1,14 @@
 /**
- * @returns {object} message
+ * @description greeting service to manupulate greeting data
+ * @return {object} message
  */
-
-exports.getHello=()=>{
-    return {'message': 'Hello'};
+module.exports = class greetingService {
+    /**
+     * @description an function which returns Hello World message
+     * @return {object} an object with string message value
+     */
+    getHello() {
+        return {'message': 'Hello'};
+    };
 };
+
