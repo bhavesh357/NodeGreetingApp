aca3062f684ee706d10970f23ba8d74d42388de3
bhavesh357 <bkadam357@gmail.com>
2020-08-12 22:15:54 +0530

    ADDED: mongoose connection event

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -14,3 +14,15 @@ mongoose.connect('mongodb://localhost:27017/greeting', {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
+
+mongoose.connection.on('connected', () => {
+    console.log('mongoose connected');
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('mongoose connection disconnected');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err.message);
+});
